90151188ce1f2022864dc4a6c1acfa1a6627781e
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 13:07:28 +0100

    Implement Register() method in AuthRepository.cs
    [v.28]
    - Implement Register method that saves Users into the table Users in DB
    - Implement method CreatePasswordHash that generates hash and salt
    - Hash and salt are generated just from the password given by the user

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using EasyClean.API.Models;
 
@@ -6,7 +7,7 @@ namespace EasyClean.API.Data
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext dataContext;
-        
+
         public AuthRepository(DataContext dataContext)
         {
             this.dataContext = dataContext;
@@ -17,9 +18,31 @@ namespace EasyClean.API.Data
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            byte[] passwordHash, passwordSalt;
+            
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            // await next two lines because we work with async methods (much better since we are dealing
+            // with a DB and inserting a value in it can take unexpectedly long)
+            await this.dataContext.Users.AddAsync(user);    // Inserts the user into the table of DB asynchronously
+            await this.dataContext.SaveChangesAsync();      // IMPORTANT: Needed after AddAsync. Otherwise user wont be saved!
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            // Generate a HMAC to compute a hash from user's password
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
         public Task<User> UserExists(string email)
