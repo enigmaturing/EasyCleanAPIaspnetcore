215b550b9ab41245e405f7c80383306e7aabeb66
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 10:42:15 +0100

    Deactivate https usage
    [v.6 Rewiewing the project files in the dotnet web api]

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace EasyClean.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
