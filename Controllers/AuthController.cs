dd3253fee6e1cc2610f26b46b0e8a3b4d4c646ee
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 14:07:24 +0100

    Implemnt endpoint in AuthController "api/register"
    [v. 31 Creating the Register method in our AuthContoller ]
    - Aim of this is, Angular will be able to make a http post request to the endpoint api/register and our api should respond creating a user in the table Users of our DB
    - The method Register makes use of the method Register exposed in the repository
    - Some ToDos (3 pieces) are still to be implemented in this endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using EasyClean.API.Data;
+using EasyClean.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyClean.API.Controllers
@@ -8,10 +10,36 @@ namespace EasyClean.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository repo;
-        
+
         public AuthController(IAuthRepository repo)
         {
             this.repo = repo;
+        }
+
+        // ToDo: Pass username and password as a DTO (data transfer object) to the following
+        // endpoint (register), insted of as two simple strings
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string email, string password)
+        {
+            // ToDo: Validate request
+
+            // Avoid having two users with the same email, one containing capital
+            // letters and one not
+            email = email.ToLower();
+            if (await this.repo.UserExists(email))
+                return BadRequest("This email was already used to register another account");
+            
+            // We can not assign the string password to userToCreate because the constructor
+            // of the User class does not contain a porperty password of type string, but two
+            // byte[] properties: passwordHash and passwordSalt. So, by the moment, we just
+            // assign a value to the property Email and let the Register method of our repo
+            // generate passwordHash and passwordSalt internally
+            var userToCreate = new User(){ Email = email };
+            var createdUser = this.repo.Register(userToCreate, password);
+
+            // Return 201 that means CreatedAtRoute, meaning that the user was created
+            // and that it is available in a certain route of the API.
+            return StatusCode(201); // ToDo: Return not only the code, but also the route where the user is available
         }
     }
 }
