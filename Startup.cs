bcd09b30fef58866aa75100b2e969145a3adaf29
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 11:19:06 +0100

    Add DataContext as a service
    [v.10 Creating the first model and DataContext]
    - Install package Microsoft.EntityFrameworkCore.Sqlite
    - Services are added in the method
    ConfigureServices() of the Startup.cs class
    - Specify also that this will be a Sqlite DB
    - We dont have a connection string to the DB by the moment -> ToDo: Add the connection string to
    the appsetings.json when we have it

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyClean.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,8 @@ namespace EasyClean.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // ToDo: In the next line, specify the correct connection string in the appsettings.json, when we have it
+            services.AddDbContext<DataContext>(x => x.UseSqlite("Connection string")); // Adds the class DataContext as a service
             services.AddControllers();
         }
 
