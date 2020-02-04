c90bf799899f8cd4b30dacd97afd27a91f2beeb4
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 14:10:45 +0100

    Use DTO to transfer email and pasword as JSON
    [v. 32 Using DTOs]
    - Create a class dtos/UserForRegisterDto.cs with Email and Password
    - Make method register in Authcontroller accept an object of this class
    - Test the transfer of the JSON object using Postman

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using EasyClean.API.Data;
+using EasyClean.API.Dtos;
 using EasyClean.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,17 +17,15 @@ namespace EasyClean.API.Controllers
             this.repo = repo;
         }
 
-        // ToDo: Pass username and password as a DTO (data transfer object) to the following
-        // endpoint (register), insted of as two simple strings
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string email, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // ToDo: Validate request
 
             // Avoid having two users with the same email, one containing capital
             // letters and one not
-            email = email.ToLower();
-            if (await this.repo.UserExists(email))
+            userForRegisterDto.Email = userForRegisterDto.Email.ToLower();
+            if (await this.repo.UserExists(userForRegisterDto.Email))
                 return BadRequest("This email was already used to register another account");
             
             // We can not assign the string password to userToCreate because the constructor
@@ -34,8 +33,8 @@ namespace EasyClean.API.Controllers
             // byte[] properties: passwordHash and passwordSalt. So, by the moment, we just
             // assign a value to the property Email and let the Register method of our repo
             // generate passwordHash and passwordSalt internally
-            var userToCreate = new User(){ Email = email };
-            var createdUser = this.repo.Register(userToCreate, password);
+            var userToCreate = new User(){ Email = userForRegisterDto.Email };
+            var createdUser = this.repo.Register(userToCreate, userForRegisterDto.Password);
 
             // Return 201 that means CreatedAtRoute, meaning that the user was created
             // and that it is available in a certain route of the API.
---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace EasyClean.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
