cefc6a95d3fe3e7836e07f59baf2bc331a6919bd
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-04 15:28:35 +0100

    Add validation of Email and Password in DTO
    [v. 33 Validation in the api]
    - Validate Email and Passowrd as not optional
    - Length of Password must be between 4 and 15 characters
    - Email must have email-format

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EasyClean.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="You must provide an email-address")]
+        [EmailAddress(ErrorMessage="Correct the email address you entered. It does not have a valid email-format")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage="You must provide a password")]
+        [StringLength(15, MinimumLength=4, ErrorMessage="You must enter a password between 4 and 15 characters")]
         public string Password { get; set; }
     }
 }
