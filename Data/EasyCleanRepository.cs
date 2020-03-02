565a526db285a87de1c0732957522a29cb644885
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-02 14:21:36 +0000

    Add a new Repository to create, delete & get users
    [v. 75 Create a new repository for our API]
    - With this repository, called EasyCleanRepository, we want to create, delete and get users from our database. In the next commit we create the Controller that calls the methods present in this Repository

---
+++ Data/EasyCleanRepository.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyClean.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyClean.API.Data
+{
+    public class EasyCleanRepository : IEasyCleanRepository
+    {
+        private readonly DataContext dataContext;
+        public EasyCleanRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            // We dont need to Add the entity asincronously because
+            // because Add(entity) only adds it to memory but do not access
+            // the DB. So there will be no simultaneous access to the DB
+            this.dataContext.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            // We dont need to Remove the entity asincronously because
+            // because Remove(entity) only removes it from memory but do not
+            // access the DB. So there will be no simultaneous access to the DB
+            this.dataContext.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            // IMPORTANT: We want to return also the photos of the user. The Photos
+            // propierty is a navigation property and therefore we have to include
+            // it specifically with Include(p => p.Photos)
+            var user = await this.dataContext.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            // IMPORTANT: We want to return also the photos of the users. The Photos
+            // propierty is a navigation property and therefore we have to include
+            // it specifically with Include(p => p.Photos)
+            var users = await this.dataContext.Users.Include(p => p.Photos).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            // Return true if saved items are greater than zero
+            return await this.dataContext.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Data/IEasyCleanRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyClean.API.Models;
+
+namespace EasyClean.API.Data
+{
+    public interface IEasyCleanRepository
+    {
+         void Add<T>(T entity) where T: class;
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
+    }
+}
