8049b66880c5ca0e9eb5f4f008be8ed4410ca087
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-05 23:20:38 +0000

    Remove Value entity and Values table from DB

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace EasyClean.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options): base(options) { }
-        public DbSet<Value> Values { get; set; }
+        
         public DbSet<User> Users { get; set; }
         public DbSet<Purchase> Purchases { get; set; }
     }
--- Migrations/20200305231735_DeleteValuesEntityAndTable.Designer.cs
+++ Migrations/20200305231735_DeleteValuesEntityAndTable.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EasyClean.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EasyClean.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200305231735_DeleteValuesEntityAndTable")]
+    partial class DeleteValuesEntityAndTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
