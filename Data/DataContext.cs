55e118ce87249ce645a819caee5694fb5b13fe39
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-25 11:19:59 +0000

    Configure DataContext to support Identity
    [v. 199 Configure the DataContext for identity]

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,17 +1,41 @@
 using EasyClean.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyClean.API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User, Role, int,
+                                                 IdentityUserClaim<int>, UserRole,
+                                                 IdentityUserLogin<int>, 
+                                                 IdentityRoleClaim<int>,
+                                                 IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions<DataContext> options): base(options) { }
-        
-        public DbSet<User> Users { get; set; }
         public DbSet<MachineUsage> MachineUsages { get; set; }
         public DbSet<MachineGroup> MachineGroups { get; set; }
         public DbSet<Tariff> Tariffs { get; set; }
         public DbSet<Topup> Topups { get; set; }
         public DbSet<Machine> Machines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(userRole => new {userRole.UserId, userRole.RoleId});
+
+                userRole.HasOne(userRole => userRole.Role)
+                .WithMany(role => role.UserRoles)
+                .HasForeignKey(userRole => userRole.RoleId)
+                .IsRequired();
+
+                userRole.HasOne(userRole => userRole.User)
+                .WithMany(role => role.UserRoles)
+                .HasForeignKey(userRole => userRole.UserId)
+                .IsRequired();
+            });
+        }
     }
 }
