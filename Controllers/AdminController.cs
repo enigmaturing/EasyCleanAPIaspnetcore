26b5cf55ede2569dc4505bd0412409333605a979
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-27 17:45:48 +0000

    Add method EditRoles() in AdminController
    [v. 209 Add the edit roles method in the API]
    . With this, a new endpoint is created (and it can only be used by users with BusinessOwnerRole): http://localhost:5000/api/admin/editroles/1
    This endpoint sets the roles of the user 1 when the new roles are passed as an array of string in the body of a POST request, like this:
    {
        "RoleNames": ["BackOfficeEmployee", "FrontDeskEmployee"]
    }

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,7 +1,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using EasyClean.API.Data;
+using EasyClean.API.Dtos;
+using EasyClean.API.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,8 +15,11 @@ namespace EasyClean.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly DataContext dataContext;
-        public AdminController(DataContext dataContext)
+        private readonly UserManager<User> userManager;
+        public AdminController(DataContext dataContext,
+                              UserManager<User> userManager)
         {
+            this.userManager = userManager;
             this.dataContext = dataContext;
         }
 
@@ -29,15 +35,43 @@ namespace EasyClean.API.Controllers
                         Id = user.Id,
                         Email = user.Email,
                         Roles = (from userRole in user.UserRoles
-                        join role in dataContext.Roles
-                        on userRole.RoleId
-                        equals role.Id
-                        select role.Name).ToList()
+                                 join role in dataContext.Roles
+                                 on userRole.RoleId
+                                 equals role.Id
+                                 select role.Name).ToList()
                     })
                     .ToListAsync();
 
 
             return Ok(userList);
+        }
+
+        [Authorize(Policy = "RequireBusinessOwnerRole")]
+        [HttpPost("editRoles/{userId}")]
+        public async Task<IActionResult> EditRoles(string userId, RoleEditDto roleEditDto)
+        {
+            // Find the user in the DB and retrieve its actual roles
+            var user = await userManager.FindByIdAsync(userId);
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            var selectedRoles = roleEditDto.RoleNames;
+            // Because selectedRoles can be null, we ask if it is null
+            // and in that case, we initialize it to an empty string
+            selectedRoles = selectedRoles ?? new string[]{};
+
+            // Add the roles that were included in the roleEditDto
+            var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+            if (!result.Succeeded)
+                return BadRequest("Failed to add the selected roles");
+
+            // Remove the roles that were not included in the roleEditDto
+            result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+            if (!result.Succeeded)
+                return BadRequest("Failed to remove the unselected roles");
+
+            // Return the active roles for the given user after modification
+            // acording to roleEditDto
+            return (Ok(await userManager.GetRolesAsync(user)));
         }
     }
 }
---
+++ Dtos/RoleEditDto.cs
@@ -0,0 +1,7 @@
+namespace EasyClean.API.Dtos
+{
+    public class RoleEditDto
+    {
+        public string[] RoleNames { get; set; }
+    }
+}
