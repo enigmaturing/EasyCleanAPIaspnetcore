dcfd5706c2c750b2b56c33acd8d9048735807653
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-27 16:17:32 +0000

    Return users along with their roles
    [v. Return the users with their roles]

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
+using EasyClean.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EasyClean.API.Controllers
 {
@@ -7,11 +11,33 @@ namespace EasyClean.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
+        private readonly DataContext dataContext;
+        public AdminController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
         [Authorize(Policy = "RequireBusinessOwnerRole")]
         [HttpGet]
-        public IActionResult GetUsersWithRoles()
+        public async Task<IActionResult> GetUsersWithRoles()
         {
-            return Ok("Only boss can access this");
+            // Return a list of users along with their roles
+            var userList = await dataContext.Users
+                    .OrderBy(x => x.Email)
+                    .Select(user => new
+                    {
+                        Id = user.Id,
+                        Email = user.Email,
+                        Roles = (from userRole in user.UserRoles
+                        join role in dataContext.Roles
+                        on userRole.RoleId
+                        equals role.Id
+                        select role.Name).ToList()
+                    })
+                    .ToListAsync();
+
+
+            return Ok(userList);
         }
     }
 }
